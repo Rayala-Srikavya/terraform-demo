00ebba0fdae44d27f30085b164aecb5b414742e0
Rayala-Srikavya <142518414+Rayala-Srikavya@users.noreply.github.com>
2025-05-06 12:06:09 +0530

    Updated main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,29 @@ terraform {
     }
   }
 }
+
+provider "snowflake" {
+  account  = "RCNZCWF-UU37435"   
+  username = "Srikavya"          
+  password = "Srpu@7330691779"   
+}
+
+# Loading YAML file
+data "yaml_file" "tables" {
+  input = file("${path.module}/snowflake-table.yaml")
+}
+
+# Creating tables dynamically from YAML configuration
+resource "snowflake_table" "tables" {
+  database = data.yaml_file.tables.snowflake.database
+  schema   = data.yaml_file.tables.snowflake.schema
+  name     = data.yaml_file.tables.snowflake.tables[0].name
+
+  dynamic "column" {
+    for_each = data.yaml_file.tables.snowflake.tables[0].columns
+    content {
+      name = column.value.name
+      type = column.value.type
+    }
+  }
+}
